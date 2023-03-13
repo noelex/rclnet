36406b67c7d15d69b1f7deee4c84ecd418c00912
noelex <noelex@outlook.jp>
2023-03-13 19:42:38 +0800

    Make sure safe handles are disposed immediately on exception so that we don't need to wait until next GC to free up unmanaged resources.

--- src/Rcl.NET/SafeHandles/SafeServiceHandle.cs
+++ src/Rcl.NET/SafeHandles/SafeServiceHandle.cs
@@ -13,25 +13,31 @@ internal unsafe class SafeServiceHandle : RclObjectHandle<rcl_service_t>
     {
         _node = node;
         *Object = rcl_get_zero_initialized_service();
-
-        var opts = rcl_service_get_default_options();
-        opts.qos = qos.ToRmwQosProfile();
-
-        var nameSize = InteropHelpers.GetUtf8BufferSize(serviceName);
-        Span<byte> nameBuffer = stackalloc byte[nameSize];
-        InteropHelpers.FillUtf8Buffer(serviceName, nameBuffer);
-
-        fixed (byte* pname = nameBuffer)
+        try
         {
-            RclException.ThrowIfNonSuccess(
-                rcl_service_init(
-                    Object,
-                    node.Object,
-                    typeSupportHandle.GetServiceTypeSupport(),
-                    pname,
-                    &opts));
-        }
+            var opts = rcl_service_get_default_options();
+            opts.qos = qos.ToRmwQosProfile();
 
+            var nameSize = InteropHelpers.GetUtf8BufferSize(serviceName);
+            Span<byte> nameBuffer = stackalloc byte[nameSize];
+            InteropHelpers.FillUtf8Buffer(serviceName, nameBuffer);
+
+            fixed (byte* pname = nameBuffer)
+            {
+                RclException.ThrowIfNonSuccess(
+                    rcl_service_init(
+                        Object,
+                        node.Object,
+                        typeSupportHandle.GetServiceTypeSupport(),
+                        pname,
+                        &opts));
+            }
+        }
+        catch
+        {
+            Dispose();
+            throw;
+        }
     }
 
     protected override void ReleaseHandleCore(rcl_service_t* ptr)
