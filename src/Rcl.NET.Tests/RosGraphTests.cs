4f42803da7235b6cdfc54d39a718117c5885c49f
noelex <noelex@outlook.jp>
2023-03-17 12:19:29 +0800

    Add tests for RosGraph.WatchAsync and friends.

--- src/Rcl.NET.Tests/RosGraphTests.cs
+++ src/Rcl.NET.Tests/RosGraphTests.cs
@@ -1,4 +1,5 @@
 ﻿using Rcl.Graph;
+using Rosidl.Messages.Builtin;
 
 namespace Rcl.NET.Tests;
 
@@ -46,5 +47,42 @@ public class RosGraphTests
 
             await Task.Delay(-1, cancellationToken);
         }
+    }
+
+    [Fact]
+    public async Task WatchForTopic()
+    {
+        await using var ctx = new RclContext(TestConfig.DefaultContextArguments);
+        using var node = ctx.CreateNode(NameGenerator.GenerateNodeName());
+
+        var targetTopic = "/" + NameGenerator.GenerateNodeName();
+        var watcher = node.Graph.TryWatchAsync((graph, e) => graph.Topics.Any(x => x.Name == targetTopic), 1000);
+
+        using var pub = node.CreatePublisher<Time>(targetTopic);
+
+        Assert.True(await watcher);
+    }
+
+    [Fact]
+    public async Task WatchTimeout()
+    {
+        await using var ctx = new RclContext(TestConfig.DefaultContextArguments);
+        using var node = ctx.CreateNode(NameGenerator.GenerateNodeName());
+
+        var targetTopic = "/" + NameGenerator.GenerateNodeName();
+        var ok = await node.Graph.TryWatchAsync((graph, e) => graph.Topics.Any(x => x.Name == targetTopic), 100);
+
+        Assert.False(ok);
+    }
+
+    [Fact]
+    public async Task WatchTimeoutThrows()
+    {
+        await using var ctx = new RclContext(TestConfig.DefaultContextArguments);
+        using var node = ctx.CreateNode(NameGenerator.GenerateNodeName());
+
+        var targetTopic = "/" + NameGenerator.GenerateNodeName();
+        await Assert.ThrowsAsync<TimeoutException>(() =>
+            node.Graph.WatchAsync((graph, e) => graph.Topics.Any(x => x.Name == targetTopic), 100));
     }
 }
