3880e64c9fe7506b5c9899cf401e9e34d2cda112
noelex <noelex@outlook.jp>
2025-09-16 17:44:39 +0800

    Disable test parallelization

--- src/Rcl.NET.Tests/_Usings.cs
+++ src/Rcl.NET.Tests/_Usings.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: DisableRuntimeMarshalling]
 [assembly: TestFramework("Rcl.NET.Tests.RclTestFramework", "Rcl.NET.Tests")]
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true)]
