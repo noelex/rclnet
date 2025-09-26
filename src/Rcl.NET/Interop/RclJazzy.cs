6c3fbafa222ec55a4252f512f037053f1db62e5c
noelex <noelex@outlook.jp>
2025-09-26 15:21:56 +0800

    Add rcl_timer_init2 API to replace rcl_timer_init for jazzy and later distros

--- src/Rcl.NET/Interop/RclJazzy.cs
+++ src/Rcl.NET/Interop/RclJazzy.cs
@@ -1,8 +1,16 @@
-﻿using Rosidl.Runtime.Interop;
-using System.Runtime.InteropServices;
-using static Rcl.Interop.RclHumble;
+﻿using System.Runtime.InteropServices;
 
 namespace Rcl.Interop;
 
-// Same typedef with iron
-using RclJazzy = RclIron;
+unsafe static class RclJazzy
+{
+    [DllImport("rcl", CallingConvention = CallingConvention.Cdecl)]
+    public static extern rcl_ret_t rcl_timer_init2(
+        RclCommon.rcl_timer_t* timer,
+        RclCommon.rcl_clock_t* clock,
+        RclCommon.rcl_context_t* context,
+        long period,
+        delegate* unmanaged[Cdecl]<RclCommon.rcl_timer_t*, long, void> callback,
+        RclCommon.rcl_allocator_t allocator,
+        bool autostart);
+}
